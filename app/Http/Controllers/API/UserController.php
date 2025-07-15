c46dfc33385b3b99dfd16c8e641df56ca23e7398
nzouda777 <nzoudarodrigue@gmail.com>
2025-07-15 17:35:00 +0100

    maj

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -65,7 +65,7 @@ class UserController extends Controller
         $user->save();
 
         return response()->json([
-            'message' => 'Password updated successfully'
+            'message' => 'Password updated 😁'
         ]);
     }
 
